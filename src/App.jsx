2f5f24a91436478b3bb94649b6cfec49e8f292c2
Zenith1997 <dabarerazenith@gmail.com>
2025-02-11 23:37:15 +0530

    Creating a styled button in mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-
-
-import './App.css'
+import { Button, styled } from "@mui/material";
+import "./App.css";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function App() {
- 
 
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    margin:5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled":{
+      backgroundColor:"gray",
+      color:"white"
+    }
+  })
   return (
-    <>
-     <div>
-      Hello
-     </div>
-    </>
-  )
+    <div>
+     <BlueButton>My button</BlueButton>
+     <BlueButton>Another button</BlueButton>
+    </div>
+  );
 }
 
-export default App
+export default App;
