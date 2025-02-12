37635363e9dc805396e8cf37a7011aa1d4dcb2c5
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-13 00:49:04 +0530

    Toolkit added to search bar which included search and icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import MailIcon from '@mui/icons-material/Mail';
+import { AppBar, Badge, Box, InputBase, styled, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import PetsIcon from '@mui/icons-material/Pets';
+import { Notifications } from '@mui/icons-material';
+const Navbar = () => {
+
+  const StyledToolbar = styled(Toolbar)(
+    {
+      display:"flex",
+      justifyContent:"space-between"
+    }
+  )
+  const Search = styled("div")(({theme})=>(
+      {
+       backgroundColor:"white",
+       padding:"0 10px",
+       borderRadius:theme.shape.borderRadius
+      }));
+
+      const Icons = styled(Box)(({theme})=>(
+        {
+          display:"flex",
+          gap:"20px",
+        
+          borderRadius:theme.shape.borderRadius
+        }));
+  
+  return (
+    <AppBar position='sticky'>
+  <StyledToolbar>
+
+  <Typography variant='h6' sx={{display:{xs:"none",sm:"block"}}}>
+    LAMA DEV
+  </Typography>
+  <PetsIcon sx={{display:{xs:"block",sm:"none",md:"none"}}}/>
+  <Search><InputBase placeholder='Search..'/></Search>
+  <Icons>
+
+  <Badge badgeContent={2} color="error">
+  <MailIcon  />
+</Badge>
+<Badge badgeContent={1} color="error">
+  <Notifications/>
+</Badge>
+  </Icons>
+  </StyledToolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
