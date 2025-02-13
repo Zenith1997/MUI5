0567a7db49a161c100366fda56f34869e43eb17e
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-13 11:21:18 +0530

    search icon added, FIxed navbar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,79 @@
-import MailIcon from '@mui/icons-material/Mail';
-import { AppBar, Avatar, Badge, Box, InputBase, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import PetsIcon from '@mui/icons-material/Pets';
-import { Notifications } from '@mui/icons-material';
+import MailIcon from "@mui/icons-material/Mail";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import PetsIcon from "@mui/icons-material/Pets";
+import SearchIcon from '@mui/icons-material/Search';
+import { Notifications } from "@mui/icons-material";
 const Navbar = () => {
+  const StyledToolbar = styled(Toolbar)({
+    display: "flex",
+    justifyContent: "space-between",
+  });
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    display:'flex',
+    gap:"5px",
+    alignItems:"center",
+    padding: "0 10px",
+    margin: "0px 20px",
+    borderRadius: theme.shape.borderRadius,
+  }));
 
-  const StyledToolbar = styled(Toolbar)(
-    {
-      display:"flex",
-      justifyContent:"space-between"
-    }
-  )
-  const Search = styled("div")(({theme})=>(
-      {
-       backgroundColor:"white",
-       padding:"0 10px",
-       borderRadius:theme.shape.borderRadius
-      }));
+  const Icons = styled(Box)(({ theme }) => ({
+    display: "none",
+    gap: "20px",
+    alignItems: "center",
+    borderRadius: theme.shape.borderRadius,
+    [theme.breakpoints.up("sm")]: {
+      display: "flex",
+    },
+  }));
 
-      const Icons = styled(Box)(({theme})=>(
-        {
-          display:"flex",
-          gap:"20px",
-          alignItems:"center",
-          borderRadius:theme.shape.borderRadius
-        }));
-  
+  const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  }));
+
   return (
-    <AppBar position='sticky'>
-  <StyledToolbar>
-
-  <Typography variant='h6' sx={{display:{xs:"none",sm:"block"}}}>
-    LAMA DEV
-  </Typography>
-  <PetsIcon sx={{display:{xs:"block",sm:"none",md:"none"}}}/>
-  <Search><InputBase placeholder='Search..'/></Search>
-  <Icons>
-
-  <Badge badgeContent={2} color="error">
-  <MailIcon  />
-</Badge>
-<Badge badgeContent={1} color="error">
-  <Notifications/>
-</Badge>
-<Avatar src=''/>
-  </Icons>
- 
-  </StyledToolbar>
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          LAMA DEV
+        </Typography>
+        <PetsIcon sx={{ display: { xs: "block", sm: "none", md: "none" } }} />
+        <Search>
+          <SearchIcon color="disabled"/>
+          <InputBase placeholder="Search.." ></InputBase>
+        </Search>
+        <Icons>
+          <Badge badgeContent={2} color="error">
+            <MailIcon />
+          </Badge>
+          <Badge badgeContent={1} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: "30px", height: "30px" }} src="" />
+        </Icons>
+        <UserBox>
+          <Avatar sx={{ width: "30px", height: "30px" }} src="" />
+          <Typography variant="span">John</Typography>
+        </UserBox>
+      </StyledToolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
