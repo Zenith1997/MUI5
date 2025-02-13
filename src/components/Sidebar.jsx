2af2ff3fb6eb7e8566111f2d360511bbd0c151c4
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-13 23:47:58 +0530

    sidebar icons for titles added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,71 @@
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
-
+import InboxIcon from '@mui/icons-material/Inbox';
+import HomeIcon from '@mui/icons-material/Home';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { Group, GroupAddRounded, GroupAddSharp, Pages, Person, Person2Outlined, Person3, Person3Outlined, Settings, Shop, ShopTwo, ShopTwoOutlined } from '@mui/icons-material';
 const Sidebar = () => {
   return (
-    <Box bgcolor={"skyblue"}flex={1} p={2} sx={{ display:{xs:"none",sm:"block"}}}>
-sidebar
+    <Box bgcolor={""}flex={1} p={2} sx={{ display:{xs:"none",sm:"block"}}}>
+    <List>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+            <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Pages />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <ShopTwoOutlined />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <GroupAddSharp />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Person/>
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+
+        </List>
     </Box>
   )
 }
