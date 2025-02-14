0af235c9ab14753fbf8198e4131a1f4bffe65f1d
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-15 01:35:28 +0530

    Rightbar contents added

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,11 +1,32 @@
 import { Box } from "@mui/material"
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#endregion',
+  ...theme.typography.body2,
+  padding: theme.spacing(5),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 
 const Rightbar = () => {
   return (
     <Box flex={1} p={2} sx={{display:{xs:"none",sm:"block"}}}> 
       <Box position={"fixed"}>
-sdffsd
+      <Box sx={{ width: '100%' }}>
+      <Stack spacing={2}>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+    </Box>
       </Box>
     </Box>
   )
