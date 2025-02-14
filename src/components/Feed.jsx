5d7f20754d0da0ea743efc1e812317b071a6ea7a
Zenith1997 <zenithanthony.official@gmail.com>
2025-02-14 23:55:47 +0530

    Minor changes to favourite icon(Made it change color to blue when clicked)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,11 @@
-import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Collapse, IconButton, styled, Typography } from '@mui/material'
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Checkbox, Collapse, IconButton, styled, Typography } from '@mui/material'
 import React from 'react'
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { ExpandMore } from '@mui/icons-material';
+import { ExpandMore, Favorite, FavoriteBorder } from '@mui/icons-material';
 const Feed = () => {
 
 
@@ -36,7 +36,7 @@ const Feed = () => {
     
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
+        <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
