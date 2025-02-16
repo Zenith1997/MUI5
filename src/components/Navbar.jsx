241fd8c8904cfc055596f03333861ea37a1f80f5
antoniodiaz1 <antoniodiaz1@ufl.edu>
2025-02-16 11:50:56 -0500

    fix: centered search bar in app bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,7 @@ const [open, setOpen] = useState(false);
   const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between",
+    position: "relative",
   });
   const Search = styled("div")(({ theme }) => ({
     backgroundColor: "white",
@@ -30,7 +31,7 @@ const [open, setOpen] = useState(false);
     gap:"5px",
     alignItems:"center",
     padding: "0 10px",
-    margin: "0px 20px",
+    
     borderRadius: theme.shape.borderRadius,
   }));
 
@@ -60,10 +61,14 @@ const [open, setOpen] = useState(false);
           LAMA DEV
         </Typography>
         <ResponsiveDrawer sx={{ display: { xs: "block", sm: "none", md: "none" } }} />
-        <Search>
-          <SearchIcon color="disabled"/>
-          <InputBase placeholder="Search.." ></InputBase>
-        </Search>
+
+        <Box sx={{ position: "absolute", left: "50%", transform: "translateX(-50%)" }}>
+          <Search>
+            <SearchIcon color="disabled" />
+            <InputBase placeholder="Search.." />
+          </Search>
+        </Box>
+        
         <Icons>
           <Badge badgeContent={2} color="error">
             <MailIcon />
